ff235bc821995946eabf72c1299a9101ba8f6a88
Zak Johansson <zak.johansson@gmail.com>
2022-01-30 14:34:40 -0800

    completed first two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const myword = '';
+const mynumber = Number();
 
-};
+const repeatString = 
+    function repeatString(myword, mynumber) {
+    if (mynumber >= 1) {
+    for (let i = 0; i < mynumber ; i++) {
+     return (myword.repeat(mynumber));
+    }
+    } else if (mynumber == 0) {
+        return ('');
+    } else if (mynumber <= -1) {
+        return ("ERROR");
+    }   
+}
 
 // Do not edit below this line
 module.exports = repeatString;
