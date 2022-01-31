96816a62589db2436f9b52e4c9f1ab74a9e755df
Zak Johansson <zak.johansson@gmail.com>
2022-01-31 11:24:07 -0800

    finished reverseString test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
+const str = '';
 const reverseString = function(str) {
+   let stringarray = str.split('');
+   let reversestring = stringarray.reverse();
+   let finstring = reversestring.toString();
+   return finstring.replaceAll(',','');
 
-};
+}
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+
+
+/* 
+1) convert string to array
+2) reverse array
+3) take array and put into string
+4) output string 
+*/
