4caf78bc4d15a495b9416a23833a9f6eacbf33b9
Zak Johansson <zak.johansson@gmail.com>
2022-02-03 14:06:14 -0800

    completed tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
+const ftoc = function(...args) {
+let fahrenheit = ((args -32) *0.555555556);
+return Math.round(fahrenheit* 10)/10;
 
 };
 
-const ctof = function() {
-
+const ctof = function(...args) {
+let celsius = ((args*1.8)+ 32);
+return Math.round(celsius*10)/10;
 };
 
 // Do not edit below this line
