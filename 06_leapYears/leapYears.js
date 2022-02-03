9d17997d446be212cb36ba575b64a185f3dfe681
Zak Johansson <zak.johansson@gmail.com>
2022-02-03 13:33:40 -0800

    complete leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,6 @@
-const leapYears = function() {
-
-};
+const leapYears = function(...arg) {
+return arg % 4 === 0 && (arg % 100 !== 0 || arg % 400 === 0);
+}
 
 // Do not edit below this line
 module.exports = leapYears;
