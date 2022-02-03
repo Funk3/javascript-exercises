ee2b16397043934fec065280f7eaf498ee56e7dd
Zak Johansson <zak.johansson@gmail.com>
2022-02-02 20:03:45 -0800

    finished removefromArray.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayinParameter,...args) {
+   let newArray = [];
+   for (let currentNumber= 0; currentNumber < arrayinParameter.length; currentNumber++) {
+      const arrayNumber = arrayinParameter[currentNumber];
+      if (args.includes(arrayNumber)) {
+         continue;
+      } else {
+         newArray.push(arrayNumber);
+      }
+   }
+   return newArray;
+}
 
-};
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+
