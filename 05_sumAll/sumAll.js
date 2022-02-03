95aee78da07180c13e266ca14dfd4c70a8a9a408
Zak Johansson <zak.johansson@gmail.com>
2022-02-03 12:22:42 -0800

    finished sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,38 @@
-const sumAll = function() {
 
-};
+const sumAll = function(start, end) {
+
+    let sum = 0;
+
+    let numberRange = [];
+
+    function addthis(item) {
+        sum += item;
+    }
+    if (start < end ){
+                for (let firstNumber = start; firstNumber <= end; firstNumber++) {
+                numberRange.push(firstNumber);
+                }
+    }   else (start > end ) ;{
+        for (let firstNumber = start; firstNumber >= end; firstNumber--) {
+            numberRange.push(firstNumber);
+    }
+                if (typeof(start) != 'number' || typeof(end) != 'number' || start <=0 || end <=0) {
+                return ('ERROR');
+                }   else  {
+                numberRange.forEach(addthis);
+                return sum;
+                }
+    }
+}
+
+
+    
+
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
