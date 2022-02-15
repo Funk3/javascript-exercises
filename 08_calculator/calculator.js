85d4bade77b8cf6ad4883a2d6f467343c6e18f8f
Zak <zak.johansson@gmail.com>
2022-02-15 12:25:25 -0800

    finished calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -2,20 +2,35 @@ const add = (...args) => args[0] + args[1];
 	
 const subtract = (...args) => args[0] - args[1];
 
-const sum = function sum() {
-  
-}
-
-const multiply = function() {
-
+const sum = function sum(array) {
+  if (array.length == 0)  {
+    return 0;
+  } else if (array.length == 1) {
+   return array[0];
+  } else {
+    let total = 0;
+    for (let i =0; i < array.length; i++) {
+      total += array[i];
+    }
+    return total;
+  }
 };
 
-const power = function() {
-	
+const multiply = function multiply(array) {
+    return array.reduce((number, newNumber) => number * newNumber);
+  };
+
+
+const power = function power(...args) {
+  return args.reduce((number, newNumber) => Math.pow(number, newNumber));
 };
 
-const factorial = function() {
-	
+const factorial = function factorial(arg) {
+  if (arg == 0) {
+    return 1;
+  } else {
+     return (arg * factorial(arg -1));
+    }
 };
 
 // Do not edit below this line
